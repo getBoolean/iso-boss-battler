5f9d4a257e9b8dd98c3e8c1b8f2315079f1335e5
getBoolean <danielwnewville@gmail.com>
2022-10-07 17:55:47 -0500

    remove extra get_node

--- MyProj/Scripts/StartMenu.gd
+++ MyProj/Scripts/StartMenu.gd
@@ -3,8 +3,6 @@ extends Node2D
 export var mainGameScene : PackedScene
 
 func _on_bt_new_game_button_up():
-    var Global = get_node("Scripts/Global.gd")
-
     var error_code = get_tree().change_scene(mainGameScene.resource_path)
     if error_code != Global.SUCCESS_CODE:
         print("ERROR: ", error_code)
