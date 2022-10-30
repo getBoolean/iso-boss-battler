09ac32db297817504d93d2ceaa4a225ad3a7e3d1
getBoolean <danielwnewville@gmail.com>
2022-10-29 23:21:55 -0500

    Remove commented out code

--- MyProj/Scripts/Player_Movement.gd
+++ MyProj/Scripts/Player_Movement.gd
@@ -12,17 +12,7 @@ func _ready():
 
 func _physics_process(_delta : float) -> void:
     # Flip sprite if mouse passes middle of the screen
-    #var screenSize = Vector2(0,0)
-    #screenSize.x = get_viewport().get_visible_rect().size.x # Get Width
-    #screenSize.y = get_viewport().get_visible_rect().size.y # Get Height
-
-    #if((get_global_mouse_position().x > screenSize.x/2)):
-    #    $Sprite.flip_h = false
-    #else:
-    #    $Sprite.flip_h = true
-
     var currPos = get_global_position()
-    
     if((get_global_mouse_position().x > currPos.x)):
         $Sprite.flip_h = false
     else:
