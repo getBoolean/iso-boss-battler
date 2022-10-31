965336795c4e12e88c799a38944f57193d02f89f
jjm0095 <jonathan.medina3@mavs.uta.edu>
2022-10-31 15:54:35 -0500

    fixed enemy movement bug

--- MyProj/Scripts/Player_detection_zone.gd
+++ MyProj/Scripts/Player_detection_zone.gd
@@ -4,12 +4,14 @@ extends Area2D
 var player = null
 
 func can_see_player():
-	return player
+    return player
 
 
 func _on_Player_detection_zone_body_entered(body):
-	player = body
+    if(body.name == "Player"):
+        player = body
 
 
 func _on_Player_detection_zone_body_exited(body):
-	player = null
+    if(body.name =="Player"):
+        player = null
