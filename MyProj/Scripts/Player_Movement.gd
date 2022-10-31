192f3612c9846cec9e156168c3b4dc385ec849cc
ShimmyShimShim <gbug89@gmail.com>
2022-10-31 17:09:18 -0500

    Boss HP
    boss now loses hp when shot, and this is reflected in the boss hp bar on the UI

--- MyProj/Scripts/Player_Movement.gd
+++ MyProj/Scripts/Player_Movement.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+signal player_health_updated(new_value, old_value)
+signal player_mp_updated(new_value, old_value)
+signal not_enough_mp()
+signal hit_boss(new_hp, old_hp)
+
 # Load the projectile scene/node
 const projectilePath = preload("res://Scenes/Projectile.tscn")
 
@@ -40,10 +45,13 @@ func _physics_process(_delta : float) -> void:
     if Input.is_action_just_pressed("primary_fire"):
         shoot()
         
-    # Damage Player, currently just a keybind.
+    # FOR TESTING Damage to Player, currently just a keybind.
     # Can change into player collides with boss projectile
     if Input.is_action_just_released("testing_dmg_player"):
         damage_player(5)
+    if Input.is_action_just_released("testing_mp_drain"):
+        use_player_mp(5)
+        
     
     # Apply movement
     # linear_velocity is the velocity vector in pixels per second.
@@ -62,18 +70,34 @@ func shoot():
     projectile.position = $Node2D/ProjectileShootLoc.global_position
     projectile.velocity = get_global_mouse_position() - projectile.position
     
-# damage_player(): applies damage to the player's 
+# damage_player(damage): applies damage to the player's 
 # HP based on the given amount of damage, kills 
 # the player if too much damage has been taken
 func damage_player(damage):
     if PLAYER_CUR_HP < damage:
         var difference = damage - PLAYER_CUR_HP
+        emit_signal("player_health_updated", 0, PLAYER_CUR_HP)
         PLAYER_CUR_HP = 0
         kill_player(difference)
     else:
-        # play damage animation
-        PLAYER_CUR_HP = PLAYER_CUR_HP - damage
-        
+        var new_hp = PLAYER_CUR_HP - damage
+        emit_signal("player_health_updated", new_hp, PLAYER_CUR_HP)
+        # play damage animation        
+        PLAYER_CUR_HP = new_hp
+
+# use_player_mp(amount): subtracts the specifiec
+# amount of mana from the player, if they have it.
+# if not, sends a signal that they don't
+func use_player_mp(amount):
+    if PLAYER_CUR_MP < amount:
+        # can't do anything, cause not enough mp
+        emit_signal("not_enough_mp")
+        pass
+    else:
+        var new_mp = PLAYER_CUR_MP - amount
+        emit_signal("player_mp_updated", new_mp, PLAYER_CUR_MP)
+        PLAYER_CUR_MP = new_mp
+
 # kill_player():
 # animates the player's death, calls the end screen
 # difference not used, but potentially useful in future
@@ -82,3 +106,7 @@ func kill_player(difference):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
+
+# passes signal through player to the UI
+func _on_Enemy_entity_boss_health_updated(new_value, old_value):
+    emit_signal("hit_boss", new_value, old_value)
--- MyProj/Scripts/UIcontroller.gd
+++ MyProj/Scripts/UIcontroller.gd
@@ -1,28 +1,32 @@
 extends Control
 
-onready var player_node = get_parent().get_parent()
-var boss_node = null
-
-var boss_hpbar = null
+onready var boss_hpbar = get_node("VBoxContainer/TopBar/BossInfo/BossBar/Boss_HPbar")
 onready var player_hpbar = get_node("VBoxContainer/BottomBar/PlayerBars/Player_HPbar")
 onready var player_mpbar = get_node("VBoxContainer/BottomBar/PlayerBars/Player_MPbar") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    player_hpbar.set_value(player_node.PLAYER_MAX_HP)
-    player_mpbar.set_value(player_node.PLAYER_MAX_MP)
-    # get boss node
-        
-    # get boss_hpbar node
-    
-    # get player_hpbar node
-    # get player_mpbar node
     pass # Replace with function body.
 
+func _on_Player_player_health_updated(new_hp, old_hp):
+    player_hpbar.set_value(new_hp)
 
+func _on_Player_player_mp_updated(new_mp, old_mp):
+    player_mpbar.set_value(new_mp)
+    
+func _on_Player_not_enough_mp():
+    # flash mp_bar
+    pass
+
+func _on_Player_hit_boss(new_hp, old_hp):
+    boss_hpbar.set_value(new_hp)    
+    
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+    pass
+    # Update HP/MP Values every frame
     #set boss_hpbarnode.Value = (bossnode.BOSS_CUR_HP) / bossnode.BOSS_MAX_HP)
-    player_hpbar.set_value(player_node.PLAYER_CUR_HP)
-    player_mpbar.set_value(player_node.PLAYER_CUR_MP)
     
+
+
+
