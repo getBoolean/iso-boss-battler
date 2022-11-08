59761e1b928935203d089aa40393873c06020b66
getBoolean <danielwnewville@gmail.com>
2022-11-08 07:49:10 +0000

    Deploying to gh-pages from @ getBoolean/iso-boss-battler@206e7f12edec9b6b3818d600d1454934b3c34cb7 🚀

--- feature/48-PlayerSprite/index.service.worker.js
+++ feature/48-PlayerSprite/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1667877809|5039530";
+const CACHE_VERSION = "1667893737|3382933";
 const CACHE_PREFIX = "MyProj-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
