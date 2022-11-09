00e2485f19b6e807eefe9da17682090018463111
AdmiralStone <40765252+AdmiralStone@users.noreply.github.com>
2022-11-09 17:33:33 +0000

    Deploying to gh-pages from @ getBoolean/iso-boss-battler@6c3eee6980515df73fa83a69ca8d563d221c8bf6 🚀

--- feature/pause-menu-54/index.service.worker.js
+++ feature/pause-menu-54/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1668008507|3347194";
+const CACHE_VERSION = "1668015199|3989638";
 const CACHE_PREFIX = "MyProj-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
