b777bc11986b2d083f8a66ac039d83c81e3ff294
getBoolean <danielwnewville@gmail.com>
2022-11-10 14:03:42 -0600

    Add NavigationAgent2D to enemy

--- MyProj/Scripts/Enemy_entity.gd
+++ MyProj/Scripts/Enemy_entity.gd
@@ -20,14 +20,34 @@ var player = null
 export var ACCELERATION = 300
 export var FRICTION = 400
 export var MAX_SPEED = 50
+export var MAX_HEALTH = 10
+
 #Object references to boss attributes
 onready var playerDetectionZone = $Player_detection_zone
 onready var attack_range = $attack_range
 onready var enemy_sprite = $EnemySprite
-export var MAX_HEALTH = 10
 
 # For Debugging purpose only
 onready var healthLabel = $Label
+
+# Navigation
+var nav_agent : NavigationAgent2D = null
+var player_zone = null
+
+export var nav_agent_radius : float = 15.0
+export var nav_optimize_path : bool = false
+export var nav_avoidance_enabled : bool = true
+
+# final navigation destination position/point
+var nav_destination : Vector2 
+# next navigation destination position/point
+var next_nav_position : Vector2 
+
+# The normal path to the destination
+var character_nav_path : Array = []
+
+# The actual path being calcuated during travel, used in the draw function
+var character_real_nav_path : Array = []
 
 #states for boss
 enum {
@@ -40,6 +60,22 @@ var velocity = Vector2.ZERO
 var state = IDLE
 var direction = 1
 
+func _ready() -> void:
+    yield(get_tree(), "idle_frame")
+    var tree = get_tree()
+    if tree.has_group("PlayerZone"):
+        player_zone = tree.get_nodes_in_group("PlayerZone")[0]
+    
+    nav_agent = $NavigationAgent2D
+    
+    # connect nav agent signal callback functions
+    nav_agent.connect("velocity_computed", self, "character_velocity_computed")
+    
+    # config nav agent attributes
+    nav_agent.max_speed = MAX_SPEED
+    nav_agent.radius = nav_agent_radius
+    nav_agent.avoidance_enabled = nav_avoidance_enabled
+
 func _physics_process(delta):
     $AnimationPlayer.play("Walk_Idle")
     healthLabel.text = str(MAX_HEALTH)
@@ -50,12 +86,16 @@ func _physics_process(delta):
             see_player()
         CHASE:
             player = playerDetectionZone.player
-            
-            if player != null:
-                var linear_direction =  (player.global_position - global_position).normalized()
-                velocity = velocity.move_toward(linear_direction * MAX_SPEED, ACCELERATION * delta)
+            if player_zone:
+                navigate()
                 if timer_node.is_stopped():
                     fire()
+            velocity = move_and_slide(velocity)
+            #if player != null:
+            #    var linear_direction =  (player.global_position - global_position).normalized()
+            #    velocity = velocity.move_toward(linear_direction * MAX_SPEED, ACCELERATION * delta)
+            #    if timer_node.is_stopped():
+            #        fire()
                 
             if velocity.x > 0:
                 enemy_sprite.flip_h = false
@@ -70,7 +110,42 @@ func _physics_process(delta):
     
     velocity = move_and_slide(velocity)
 
+
+func navigate() -> void:
+    if player_zone:
+        nav_agent.set_target_location(player_zone.global_position)
+        character_nav_path = Navigation2DServer.map_get_path(nav_agent.get_navigation_map(), global_position, nav_destination, nav_optimize_path)
+        character_real_nav_path.clear()
+        
+        next_nav_position = nav_agent.get_next_location()
+        character_real_nav_path.push_back(next_nav_position)
+        var desired_velocity = global_position.direction_to(next_nav_position) * MAX_SPEED
+        nav_agent.set_velocity(desired_velocity)
+
+
+func set_navigation_position(nav_position_to_set : Vector2) -> void:
+    nav_destination = nav_position_to_set
     
+    # set the new character target location
+    nav_agent.set_target_location(nav_destination)
+    
+    # calculate a new map path with the server using character nav agent map and new nav destination
+    character_nav_path = Navigation2DServer.map_get_path(nav_agent.get_navigation_map(), global_position, nav_destination, nav_optimize_path)
+    
+    # clear the old real nav path, used for draw function
+    character_real_nav_path.clear()
+    
+func character_velocity_computed(calculated_velocity : Vector2) -> void:
+    velocity = calculated_velocity
+    
+    # check if nav agent target is reached
+    if !nav_agent.is_target_reached():
+        # move and slide with the new calculated velocity
+        velocity = move_and_slide(velocity)
+    else:
+        # if reached target, stand at the closest point in the navigation map
+        global_position = Navigation2DServer.map_get_closest_point(nav_agent.get_navigation_map(), global_position)
+
 
 func damage_boss(damage):
     if BOSS_CUR_HP <= damage:
