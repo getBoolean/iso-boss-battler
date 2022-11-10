649f46bc5322235be60624aa70ce94d5b0cefe62
getBoolean <danielwnewville@gmail.com>
2022-11-09 19:28:58 -0600

    Revert "Merge branch 'feature/pause-menu-54' of https://github.com/getBoolean/iso-boss-battler into feature/pause-menu-54"
    This reverts commit bb26a3cca85f2361eba95d92920f608494c951f5, reversing
    changes made to c990f7de816e33d2f8cf5e45e2021877842728ca.

--- MyProj/Scripts/PauseMenu.gd
+++ MyProj/Scripts/PauseMenu.gd
@@ -3,21 +3,13 @@ var mainMenuScene = load("res://Scenes/StartMenu.tscn")
 onready var root = get_tree().current_scene
 onready var fg_layer = root.get_node("Foreground Layer")
 
-func _input(event):
-    if event.is_action_pressed("pause_game") && get_tree().paused:
-        hide()
-        Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-        fg_layer.get_node("Crosshair").visible = true
-        get_tree().paused = false
-    
-    elif(event.is_action_pressed("pause_game") && !get_tree().paused):
-        show()
-        # Enabble custom mouse pointer and enable system pointer
-        fg_layer.get_node("Crosshair").visible = false
-        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-        # unpause the game tree
-        get_tree().paused = !get_tree().paused
+func _ready():
+    var os = OS.get_name()
+    if (os == "Android" or os == "iOS" or os == "HTML5"):
+        hide()
+
+
 
 
 func _on_bt_exit_to_desktop_button_up():
@@ -41,4 +33,13 @@ func _on_bt_resume_game_button_up():
     get_tree().paused = false
 
 
+func _on_Player_paused():
+    show()
+    # Disable custom mouse pointer and enable system pointer
+    fg_layer.get_node("Crosshair").visible = false
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+    # Pause the game tree
+    get_tree().paused = !get_tree().paused
     
+
