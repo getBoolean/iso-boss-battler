d80ca5ec3b919840659a602d510f75a855aa7675
jjm0095 <54652378+jjm0095@users.noreply.github.com>
2022-11-15 21:12:16 -0600

    Added more animations to the boss
    added spawn, death, phase transition, and phase 2 idle animation. Boss body will remain after being killed. Phase 2 animation will trigger when boss gets to <= 50% HP

--- MyProj/Scripts/Enemy_entity.gd
+++ MyProj/Scripts/Enemy_entity.gd
@@ -11,8 +11,8 @@ onready var timer_node = $fire_delay_timer
 export var fire_delay_rate = 0.4
 
 #Boss Health Values
-export var BOSS_MAX_HP = 200
-export onready var BOSS_CUR_HP = 200
+export var BOSS_MAX_HP = 10
+export onready var BOSS_CUR_HP = 10
 
 var player = null
 
@@ -23,32 +23,52 @@ export var MAX_SPEED = 125
 #Object references to boss attributes
 onready var playerDetectionZone = $Player_detection_zone
 onready var attack_range = $attack_range
-onready var enemy_sprite = $EnemySprite
+onready var enemy_sprite = $AnimatedSprite
+onready var PHASE = 1
+onready var phase_changed = 0
 export var MAX_HEALTH = 10
+
+onready var anim_player = $AnimatedSprite/AnimationPlayer
+
 
 # For Debugging purpose only
 onready var healthLabel = $Label
 
+
 #states for boss
 enum {
-    IDLE,
+    NUETRAL,
+    ACTIVATE,
     CHASE,
-    ATTACK
+    ATTACKING,
+    DEAD,
+    PHASE_CHANGE
 }
 var velocity = Vector2.ZERO
 
-var state = IDLE
+var state = NUETRAL
 var direction = 1
+#var distance_to_player = global_position.direction_to(playerDetectionZone.player.global_position)
 
 func _physics_process(delta):
-    $AnimationPlayer.play("Walk_Idle")
     healthLabel.text = str(MAX_HEALTH)
     match state:
-        IDLE:
-            #enemy_sprite.play("Idle")
-            velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
+        NUETRAL:
+            
+            anim_player.play("Spawn")
+            anim_player.stop(false)
             see_player()
+        ACTIVATE:
+            anim_player.play("Spawn")
+            yield(anim_player,"animation_finished")
+            state = CHASE
         CHASE:
+            
+            if PHASE == 1:
+                anim_player.play("Idle")
+            elif PHASE == 2:
+                anim_player.play("Idle_Phase_2")
+            
             player = playerDetectionZone.player
             
             if player != null:
@@ -61,14 +81,22 @@ func _physics_process(delta):
                 enemy_sprite.flip_h = false
             if velocity.x < 0:
                 enemy_sprite.flip_h = true
-            see_player()
-        ATTACK:
-            velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
-            print("attacked player")
-            see_player()
-            #enemy_sprite.play("Attack")
+            
+            if BOSS_CUR_HP <= .5 * BOSS_MAX_HP and phase_changed == 0:
+                phase_changed = 1
+                state = PHASE_CHANGE
+            velocity = move_and_slide(velocity)
+        DEAD:
+            MAX_SPEED = 0
+        PHASE_CHANGE:
+            MAX_SPEED = 0
+            anim_player.play("Phase_Change")
+            yield(anim_player,"animation_finished")
+            MAX_SPEED = 125
+            PHASE = 2
+            state = CHASE
     
-    velocity = move_and_slide(velocity)
+    #velocity = move_and_slide(velocity)
 
     
 
@@ -84,27 +112,25 @@ func damage_boss(damage):
         # play damage animation        
         BOSS_CUR_HP = new_hp
         
+        
 # kill_boss():
 # animates the boss's death, calls the win screen
 # difference not used, but potentially useful in future
 func kill_boss(_difference):
     emit_signal("boss_died", _difference)
-    queue_free()
+    anim_player.play("Death")
+    state = DEAD
+    #queue_free()
     pass
 
 
 func see_player():
     if playerDetectionZone.can_see_player():
-        state = CHASE
-    else :
-        state = IDLE
+        player = playerDetectionZone.player
+        if state == NUETRAL:
+            state = ACTIVATE
+       
         
-func can_attack_player():
-    if attack_range.can_attack_player():
-        state = ATTACK
-    else: 
-        state = IDLE
-
 # Player Projectile collides with boss
 func _on_Area2D_area_entered(area:Area2D):
     if area.name == "bullet_area" and area.get_parent().projectile_owner == "Player":
