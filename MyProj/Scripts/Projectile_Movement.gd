b38b062231b0c5036b572f407425b5e7a3664699
ShimmyShimShim <gbug89@gmail.com>
2022-11-16 13:26:06 -0600

    added timed despawning
    projectiles despawn after 2.5 seconds, instead of when they go off screen

--- MyProj/Scripts/Projectile_Movement.gd
+++ MyProj/Scripts/Projectile_Movement.gd
@@ -4,9 +4,11 @@ var velocity = Vector2()
 var speed = 700
 var projectile_owner = null
 
+const PROJECTILE_EXPLOSION = preload("res://Scenes/projectile1_explosion.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    $projectile_despawn_timer.start()
     pass
 
 
@@ -17,5 +19,21 @@ func _physics_process(delta):
         queue_free()
 
 # Despawn the instance once the sprite has exited the screen
-func _on_VisibilityNotifier2D_screen_exited():
+#func _on_VisibilityNotifier2D_screen_exited():
+#    queue_free()
+    
+func _on_projectile_despawn_timer_timeout():
     queue_free()
+
+# spawn explosion after being removed
+func _on_Node2D_tree_exiting():
+    var explosion = PROJECTILE_EXPLOSION.instance()
+    var current_rotation = self.rotation_degrees
+    explosion.position.x = self.position.x
+    explosion.position.y = self.position.y
+    explosion.rotation_degrees = current_rotation
+    get_parent().call_deferred("add_child", explosion)
+    pass # Replace with function body.
+
+
+
