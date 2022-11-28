67d7aa37a1c3caaf346ceaddd91648469816db59
getBoolean <danielwnewville@gmail.com>
2022-11-28 02:12:14 -0600

    Call base class functions

--- MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStates/ChaseAttackState.gd
+++ MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStates/ChaseAttackState.gd
@@ -17,3 +17,30 @@ func attack(_delta: float) -> void:
     if attack_cooldown_timer.is_stopped():
         attack_cooldown_timer.start(CHASE_ATTACK_DELAY)
         enemy.fire()
+
+# Receives events from the `_unhandled_input()` callback.
+func handle_input(event: InputEvent) -> void:
+    .handle_input(event)
+
+
+# Corresponds to the `_process()` callback.
+func update(delta: float) -> void:
+    .update(delta)
+
+
+# Corresponds to the `_physics_process()` callback.
+func physics_update(delta: float) -> void:
+    .physics_update(delta)
+
+
+# Called by the state machine upon changing the active state. The `msg` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(msg := {}) -> void:
+    # We must declare all the properties we access through `enemy` in the `EnemyEntity.gd` script.
+    .enter(msg)
+
+
+# Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+    .exit()
--- MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStates/KeepDistanceAttackState.gd
+++ MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStates/KeepDistanceAttackState.gd
@@ -5,28 +5,28 @@ extends AttackState
 onready var state_timer = $StateTimer
 
 # Receives events from the `_unhandled_input()` callback.
-func handle_input(_event: InputEvent) -> void:
-    pass
+func handle_input(event: InputEvent) -> void:
+    .handle_input(event)
 
 
 # Corresponds to the `_process()` callback.
-func update(_delta: float) -> void:
-    pass
+func update(delta: float) -> void:
+    .update(delta)
 
 
 # Corresponds to the `_physics_process()` callback.
-func physics_update(_delta: float) -> void:
-    pass
+func physics_update(delta: float) -> void:
+    .physics_update(delta)
 
 
 # Called by the state machine upon changing the active state. The `msg` parameter
 # is a dictionary with arbitrary data the state can use to initialize itself.
-func enter(_msg := {}) -> void:
+func enter(msg := {}) -> void:
     # We must declare all the properties we access through `enemy` in the `EnemyEntity.gd` script.
-    pass
+    .enter(msg)
 
 
 # Called by the state machine before changing the active state. Use this function
 # to clean up the state.
 func exit() -> void:
-    pass
+    .exit()
