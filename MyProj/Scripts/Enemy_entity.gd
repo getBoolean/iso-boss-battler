52e4847d5fe3f48383ed7909d9d2981e1e1d3944
getBoolean <danielwnewville@gmail.com>
2022-11-27 23:22:36 -0600

    Add comments

--- MyProj/Scripts/Enemy_entity.gd
+++ MyProj/Scripts/Enemy_entity.gd
@@ -57,6 +57,11 @@ func see_player():
 func _on_Area2D_area_entered(area: Area2D):
     if area.name == "bullet_area" and area.get_parent().projectile_owner == "Player":
         area.get_parent().queue_free()
+        # The boss should only take damage if they have this function.
+        # Only `AttackState` has this method, so the boss is invulnerable in
+        # other states.
+        # We need to use methods on the current state to encapsulate state
+        # transition logic
         if state_machine.state.has_method('damage_boss'):
             state_machine.state.damage_boss(5)
         
