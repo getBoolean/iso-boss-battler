fd1a5abb414c3e48e9d9fd70e176f5ddfab49b98
jjm0095 <54652378+jjm0095@users.noreply.github.com>
2022-11-28 17:56:46 -0600

    added generator creator

--- MyProj/Scripts/projectile_spawner.gd
+++ MyProj/Scripts/projectile_spawner.gd
@@ -5,19 +5,22 @@ const projectile_scene = preload("res://Scenes/Enemy_Projectile.tscn")
 #references to child nodes of timer and rotator
 onready var timer = $Timer
 onready var rotator = $Rotator
+onready var lifetime = $lifetime
 
 #variables for the spawner
 var rot_speed
 var timer_wait_time
 var spawn_point_num
 var r
+var life_length
 
 #initialize using arguments from constructor
-func init(rot, timer_arg, spawn_num, radius):
+func init(rot, timer_arg, spawn_num, radius, life):
     rot_speed = rot
     timer_wait_time = timer_arg
     spawn_point_num = spawn_num
     r = radius
+    life_length = life
 
 func _ready():
     #sets value on circle for spawn points
@@ -33,6 +36,9 @@ func _ready():
         
     timer.wait_time = timer_wait_time
     timer.start()
+    lifetime.set_wait_time(life_length)
+    lifetime.start()
+    
     
 func _process(delta):
     var new_rot = rotator.rotation_degrees + rot_speed * delta
@@ -49,3 +55,7 @@ func _on_Timer_timeout():
         projectile.projectile_owner = "Enemy_entity"
         projectile.scale.x = 1.5
         projectile.scale.y = 1.5
+
+
+func _on_lifetime_timeout():
+    queue_free()
