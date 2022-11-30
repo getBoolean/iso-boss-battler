4285fa30f28da4b2ba293abe7e2b2d1df82f668c
ShimmyShimShim <gbug89@gmail.com>
2022-11-30 15:58:44 -0600

    removed comments and pass

--- MyProj/Scenes/Level1/PlayerCamera.gd
+++ MyProj/Scenes/Level1/PlayerCamera.gd
@@ -1,19 +1,14 @@
 extends Camera2D
 
+onready var player_node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var player_node
-
+# get reference to player node when put into scene scene starts
 func _on_PlayerCamera_tree_entered():
     player_node = get_parent().get_node("PlayerLayer/Player")
-    pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# set camera position to player position
 func _process(_delta):
     self.position = player_node.position
-    pass
 
 
 
