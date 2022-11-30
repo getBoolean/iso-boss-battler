b6737b7cc6d83e684e600589d5dcd2d5b621d6a8
ShimmyShimShim <gbug89@gmail.com>
2022-11-30 12:39:20 -0600

    added slight camera smoothing

--- MyProj/Scenes/Level2/PlayerCamera.gd
+++ MyProj/Scenes/Level2/PlayerCamera.gd
@@ -4,15 +4,15 @@ extends Camera2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var player_node2
+var player_node
 
 func _on_PlayerCamera_tree_entered():
-    player_node2 = get_parent().get_node("PlayerLayer/Player")
+    player_node = get_parent().get_node("PlayerLayer/Player")
     pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-    self.position = player_node2.position
+    self.position = player_node.position
     pass
 
 
