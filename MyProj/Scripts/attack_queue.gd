6b4eda773c16fc80089a62634153447c4f2cd802
getBoolean <danielwnewville@gmail.com>
2022-12-01 14:36:12 -0600

    Remove debug print

--- MyProj/Scripts/attack_queue.gd
+++ MyProj/Scripts/attack_queue.gd
@@ -11,16 +11,12 @@ func _ready():
         rng.randomize()
         var value = rng.randi_range(1,4)
         queue.append(value)
-        print(value)
     
 
 func fire_pattern():
     var ret_val = queue.pop_front()
     rng.randomize()
     queue.append(rng.randi_range(1,3))
-    print("new queue:")
-    for i in range(12):
-        print(queue[i])
     return ret_val
         
 
