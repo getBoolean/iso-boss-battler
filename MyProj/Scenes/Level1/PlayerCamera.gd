b0f8285170f7461036c083d5178391bf5edda356
ShimmyShimShim <gbug89@gmail.com>
2022-11-30 18:25:42 -0600

    fixing conflict with main

--- MyProj/Scenes/Level1/PlayerCamera.gd
+++ MyProj/Scenes/Level1/PlayerCamera.gd
@@ -1,16 +1,14 @@
 extends Camera2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var player_node2
+onready var player_node2
 
+# get reference to player node when put into scene scene starts
 func _on_PlayerCamera_tree_entered():
     player_node2 = get_parent().get_node("PlayerLayer/Player")
     pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# set camera position to player position
 func _process(_delta):
     self.position = player_node2.position
     pass
