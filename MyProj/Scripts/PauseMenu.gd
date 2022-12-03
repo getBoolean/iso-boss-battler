203cf5f3701be551d424678508f334e5a68047b2
ShimmyShimShim <gbug89@gmail.com>
2022-12-03 17:40:31 -0600

    made sfx quieter and added sfx to pause menu

--- MyProj/Scripts/PauseMenu.gd
+++ MyProj/Scripts/PauseMenu.gd
@@ -3,6 +3,10 @@ var mainMenuScene = load("res://Scenes/StartMenu.tscn")
 onready var root = get_tree().current_scene
 onready var fg_layer = root.get_node("ForegroundLayer")
 
+onready var hover_sfx = $hover_sound
+onready var down_sfx = $down_sound
+onready var up_sfx = $up_sound
+
 func _input(event):
     if event.is_action_pressed("pause_game") && get_tree().paused:
         hide()
@@ -21,10 +25,14 @@ func _input(event):
 
 
 func _on_bt_exit_to_desktop_button_up():
+    up_sfx.play()
+    yield(up_sfx, "finished")
     get_tree().quit()
 
 
 func _on_bt_quit_main_to_menu_button_up():
+    up_sfx.play()
+    yield(up_sfx, "finished")
     hide()
     get_tree().paused = false
     Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -35,6 +43,8 @@ func _on_bt_quit_main_to_menu_button_up():
 
 
 func _on_bt_resume_game_button_up():
+    up_sfx.play()
+    yield(up_sfx, "finished")
     hide()
     Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
     fg_layer.get_node("Crosshair").visible = true
@@ -50,3 +60,17 @@ func _on_Player_paused():
     # Pause the game tree
     get_tree().paused = !get_tree().paused
     
+# SFX Down
+func _on_bt_resume_game_button_down():
+    down_sfx.play()
+func _on_bt_quit_main_to_menu_button_down():
+    down_sfx.play()
+func _on_bt_exit_to_desktop_button_down():
+    down_sfx.play()
+# Hover
+func _on_bt_resume_game_mouse_entered():
+    hover_sfx.play()
+func _on_bt_quit_main_to_menu_mouse_entered():
+    hover_sfx.play()
+func _on_bt_exit_to_desktop_mouse_entered():
+    hover_sfx.play()
