8cdb0504b4c92156e13cb2a3bd0bab28e1d0b04e
ShimmyShimShim <gbug89@gmail.com>
2022-12-05 13:17:08 -0600

    laser temp fix

--- MyProj/Scripts/StateMachine/GolemBossEnemy/golemBossStateMachine.gd
+++ MyProj/Scripts/StateMachine/GolemBossEnemy/golemBossStateMachine.gd
@@ -4,7 +4,9 @@ extends StateMachine
 # Player Projectile collides with boss
 func _on_Area2D_area_entered(area: Area2D):
     if area.name == "damage_area" and area.get_parent().attack_owner == "Player":
-        area.get_parent().queue_free()
+        var proj = area.get_parent()
+        if "Projectile" in proj.name:
+            proj.queue_free()
         # The boss should only take damage if they have this function.
         # Only `AttackState` has this method, so the boss is invulnerable in
         # other states.
--- MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStateMachine.gd
+++ MyProj/Scripts/StateMachine/RockBossEnemy/RockBossStateMachine.gd
@@ -1,16 +1,11 @@
 extends StateMachine
 
-# If damage of charge attack exceeds this number, play different
-# sound effect for boss taking damage
-var ouch_dmg_threshold = 8
-
-onready var ouch_sfx = get_parent().get_node("ouch")
-onready var big_ouch_sfx = get_parent().get_node("big_ouch")
-
 # Player Projectile collides with boss
 func _on_Area2D_area_entered(area: Area2D):
     if area.name == "damage_area" and area.get_parent().attack_owner == "Player":
-        area.get_parent().queue_free()
+        var proj = area.get_parent()
+        if "Projectile" in proj.name:
+            proj.queue_free()
         # The boss should only take damage if they have this function.
         # Only `AttackState` has this method, so the boss is invulnerable in
         # other states.
@@ -19,10 +14,6 @@ func _on_Area2D_area_entered(area: Area2D):
         if state.has_method('damage_boss'):
             var attack: Attack = area.get_parent()
             state.damage_boss(attack.damage)
-            if attack.damage > ouch_dmg_threshold:
-                big_ouch_sfx.play()
-            else:
-                ouch_sfx.play()
 
 # only lets boss transisition to activate when player can see boss
 func _on_BossVisibilityNotif_screen_entered():
