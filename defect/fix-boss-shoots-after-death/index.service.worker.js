452aa9e2064bb9ac7f08a20f412078f1fe56074a
getBoolean <danielwnewville@gmail.com>
2022-12-07 04:38:51 +0000

    Deploying to gh-pages from @ getBoolean/iso-boss-battler@0c175506356baca0c5af9c90eab993bbb0ac9086 🚀

--- defect/fix-boss-shoots-after-death/index.service.worker.js
+++ defect/fix-boss-shoots-after-death/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1670387747|18566814";
+const CACHE_VERSION = "1670387902|20898055";
 const CACHE_PREFIX = "MyProj-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
