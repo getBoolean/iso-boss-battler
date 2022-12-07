de9608ee1ef7209543447d53d56d1d123c52f806
ShimmyShimShim <gbug89@gmail.com>
2022-12-07 20:52:14 +0000

    Deploying to gh-pages from @ getBoolean/iso-boss-battler@9272bfee3300ddb8ce99db2c7e0f34c4f00033a6 🚀

--- defect/BossProjectileSFXfix/index.service.worker.js
+++ defect/BossProjectileSFXfix/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1670444948|19168561";
+const CACHE_VERSION = "1670446301|19439578";
 const CACHE_PREFIX = "Varvaros Invasio-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
