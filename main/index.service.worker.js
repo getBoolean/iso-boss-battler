9614be049a6dfd1bb22706214551ba6a81b03a30
getBoolean <getboolean@gmail.com>
2023-12-06 04:14:21 +0000

    Deploying to gh-pages from @ getBoolean/iso-boss-battler@68eeeb09738bb10bfb5d5c97d90f6955dfc180af 🚀

--- main/index.service.worker.js
+++ main/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677900784|21399048";
+const CACHE_VERSION = "1701836034|12029872";
 const CACHE_PREFIX = "Varvaros Invasio-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
